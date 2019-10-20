67217d0bc870980f4659a951a6926e0a2cd54cea
pedro-lamego <pedrownlamego@hotmail.com>
2019-10-20 16:09:13 +0100

    ficheiros novos!

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -5,68 +5,45 @@ import time
   
 class SearchProblem:
 
-<<<<<<< HEAD
-  def __init__(self, goal, model, auxheur = []):
-    self.goal = goal
-    self.model = model 
-    self.auxheur = auxheur 
-    pass
+	def __init__(self, goal, model, auxheur = []):
+		self.goal = goal
+		self.model = model 
+		self.auxheur = auxheur 
+		pass
 
-  def search(self, init, limitexp = 2000):
-    g_cost = 0
+	def search(self, init, limitexp = 2000):
+		g_cost = 0 
+		self.a_star(init)
 
+	def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
+		return []
 
-  
+	def a_star(init):
+		openList = self.openListInit(init)
+		closeList = []
+		#goalList = self.goalListInit()
+		print(openList)
+		print(closeList)
 
-  def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
-    ##
-    ## to implement
-    return []
+	def openListInit(init):
+		openList = []
+		for i in range(len(init)):
+			openList += [Node(init[i], None, 0, 0)]
+		return openList
 
-
-
-=======
-  	def __init__(self, goal, model, auxheur = []):
-    	self.goal = goal;
-    	self.model = model;
-    	self.auxheur = auxheur;
-    	pass
-
-  	def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
-   		openList = initOpenList(self.init, self.model)
-   		closedList = initClosedList(init)
-   		while len(openList) != 0 :
-
-
-
-    	return closedList
-
-    def initOpenList(init, matrix):
-    	openList = []
-    	for i in range(len(init)):
-    		openList += matrix[init[i]]
-    	return openList
-
-    def initClosedList(init):
-    	closedList = []
-    	closedList += [[], init]
-    	return closedList
-
-    def selectChild(fater ,listAdj, goal):
-    	distance = distance(listAdj[0], goal)   
-    	point = listAdj[0]
-    	for i in range(len(listAdj)- 1):
-    		j = i + 1
-    		newDistance = distance(listAdj[j], goal)
-    		if distance < newDistance:
-    			distance = newDistance
-    			point = listAdj[j]
+	def goalListInit():
+		goalList = []
+		for i in range(len(self.goal)):
+			goalList += [Node(goal[i], None, 0, 0)]
+		return goalList
 
 
+class Node:
+	def __init__(self, index, father, g, h):
+		self.father = father;
+		self.g = g
+		self.h = h
+		self.f = g + h 
+		self.index = index
 
-    def distance(point, goal):
-    	return Math.sqrt((point[0]-goal[0]).pow(2)+(point[1]-goal[1]).pow(2))
 
-    
-  
->>>>>>> 98fa640731e2a2c00315cde93b1f83049db31aad
