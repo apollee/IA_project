aea5b5e5d61d7fbf14016eb9707575e57071dfcb
pedro-lamego <pedrownlamego@hotmail.com>
2019-10-21 11:34:43 +0100

    did a bit

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -11,27 +11,41 @@ class SearchProblem:
 		self.auxheur = auxheur 
 		pass
 
+	def getCoords(self, index):
+		return self.auxheur[index]
+
+	def getAdjs(self, index):
+		return self.model[index]
+
+
 	def search(self, init, limitexp = 2000):
 		g_cost = 0 
 		self.a_star(init)
 
 	def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
+		self.a_star(init)
 		return []
 
-	def a_star(init):
+	def a_star(self, init):
 		openList = self.openListInit(init)
-		closeList = []
+		closeList = self.closeList()
 		#goalList = self.goalListInit()
-		print(openList)
-		print(closeList)
 
-	def openListInit(init):
+
+
+
+
+	def openListInit(self, init):
 		openList = []
 		for i in range(len(init)):
 			openList += [Node(init[i], None, 0, 0)]
 		return openList
 
-	def goalListInit():
+	def closedListInit(self, init):
+		closedList = [[[],init]]
+		return closedList
+
+	def goalListInit(self):
 		goalList = []
 		for i in range(len(self.goal)):
 			goalList += [Node(goal[i], None, 0, 0)]
@@ -45,5 +59,13 @@ class Node:
 		self.h = h
 		self.f = g + h 
 		self.index = index
+	
+	def getFather(self):
+		return self.father
+
+	def getIndex(self):
+		return self.index
+
+
 
 
