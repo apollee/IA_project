e7e7e32ad46468b7083634ccf07c808f9739d77f
apollee <mariabeatriz.dongamartins@gmail.com>
2019-10-24 21:52:43 +0100

    created functions for node class and started using them to make code more readable

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -4,7 +4,6 @@ import time
 from itertools import product
 
 
-
 class SearchProblem:
 
 	def __init__(self, goal, model, auxheur = []):
@@ -12,15 +11,10 @@ class SearchProblem:
 		self.model = model 
 		self.auxheur = auxheur 
 		self.matrix = floydWarshall(self.model)
-		self.expansion = 0
-		#self.createNodes()
 		pass
 
-	def getCoords(self, index):
-		return self.auxheur[index]
-
-	def getAdjs(self, index):
-		return self.model[index]
+	def neighbors(self, index):
+		return self.model[index] 
 
 	def search(self, init, limitexp = 2000):
 		g_cost = 0 
@@ -31,38 +25,8 @@ class SearchProblem:
 			return self.a_star_1_2(init, self.model, self.goal, tickets)
 		elif(len(init) == 3):
 			return self.a_star_3_4(init, self.model, self.goal, tickets, self.matrix)
-		return []
-
-	def createAnswer(self, list_nodes):
-		final_list = []
-		for i in range(len(list_nodes[0])):
-			list_for_onepath = []
-			list_indexes = []
-			list_transports = []
-			for j in range(len(list_nodes)):
-				if(i != 0):
-					list_transports.append(list_nodes[j][i].transport_index[0])
-				list_indexes.append(list_nodes[j][i].transport_index[1])
-			list_for_onepath.append(list_transports)
-			list_for_onepath.append(list_indexes)
-			final_list.append(list_for_onepath)
-
-		
-		#print(final_list)
-		print(self.expansion)
-
-		return final_list
-	
-	def createNodes(self):
-		node_list = []
-		for i in range(0, 114):
-			node_list.append(Node(i, None, 0, 0))
-		
-		return node_list
-	
-	#auxiliary functions
-	def start_node_answer(self, list):
-		return [[], [list[0].index]]
+		else:
+			return []
 
 	def a_star_1_2(self, init, model, goal, tickets):
 		goal = goal[0]
@@ -74,7 +38,6 @@ class SearchProblem:
 		openset.add(current)
 
 		while openset:
-
 			min = 1000
 			for node in openset: 
 				value = node.g + self.heuristic(goal, node)
@@ -82,36 +45,35 @@ class SearchProblem:
 					min = value
 					current = node
 
-			if current.transport_index[1] == goal:
+			if current.getIndex() == goal:
 				path = []
-				while current.father:
+				while current.getFather():
 					path.append(current.transport_index)
-					current = current.father
+					current = current.getFather()
 				path.append(current.transport_index)
 				print(path[::-1])
 				return path[::-1]
 		
 			openset.remove(current)
-			for node in self.neighbors(current.transport_index[1]):
+			for node in self.neighbors(current.getIndex()):
 				node = Node(node, current, current.tickets)
-				node.reduceTickets(node.transport_index[0])
-				if(node.tickets[node.transport_index[0]] < 0):
+				node.reduceTickets(node.getTransport())
+				if(node.tickets[node.getTransport()] < 0):
 					continue
 				if node in openset:
 					new_g = current.g + 1
 					if node.g > new_g:
 						node.g = new_g
-						node.father = current
+						node.setFather(current)
 				else:
 					node.g = current.g + 1
 					node.h = self.heuristic(goal, node)
-					node.father = current
+					node.setFather(current)
 					openset.add(node)
 
 	def a_star_3_4(self, init, model, goal, tickets, matrix):
 		maxDistance = max(matrix[init[0]][goal[0]], matrix[init[1]][goal[1]], matrix[init[2]][goal[2]])
 		#for em volta disto!!!
-
 
 		while maxDistance < 10:
 			copspathList = []
@@ -139,8 +101,7 @@ class SearchProblem:
 		while maxDistance != -1:
 			length = len(openset)
 			while length:
-				for child in self.neighbors(openset[0][-1].transport_index[1]):
-					self.expansion += 1
+				for child in self.neighbors(openset[0][-1].getIndex()):
 					if(self.matrix[child[1]][goal] <= maxDistance or child[1] == goal):
 						newNode = Node([child[0], child[1]], openset[0][-1], tickets)
 						newList = openset[0].copy()	
@@ -152,25 +113,23 @@ class SearchProblem:
 		return openset
 
 	def detectCollisions(self, copspathList, tickets):
-
 		for lst in product(copspathList[0], copspathList[1], copspathList[2]):
 			flag_collisions = 1
 			newTickets = tickets.copy()
 			for j in range(len(lst[0])):
-				if lst[0][j].transport_index[1] == lst[1][j].transport_index[1]:
+				if lst[0][j].getIndex() == lst[1][j].getIndex():
 					flag_collisions = 0
 					break
-				if lst[0][j].transport_index[1] == lst[2][j].transport_index[1]:
+				if lst[0][j].getIndex() == lst[2][j].getIndex():
 					flag_collisions = 0
 					break 
-				if lst[1][j].transport_index[1] == lst[2][j].transport_index[1]:
+				if lst[1][j].getIndex() == lst[2][j].getIndex():
 					flag_collisions = 0
 					break
-				#print(lst[0][j].transport_index[0])
-				if(lst[0][j].transport_index[0] != []):
-					newTickets = self.reduceTickets(lst[0][j].transport_index[0], newTickets)
-					newTickets = self.reduceTickets(lst[1][j].transport_index[0], newTickets)
-					newTickets = self.reduceTickets(lst[2][j].transport_index[0], newTickets)
+				if(lst[0][j].getTransport() != []):
+					newTickets = self.reduceTickets(lst[0][j].getTransport(), newTickets)
+					newTickets = self.reduceTickets(lst[1][j].getTransport(), newTickets)
+					newTickets = self.reduceTickets(lst[2][j].getTransport(), newTickets)
 				if(not self.checkTickets(newTickets)):
 					flag_collisions = 0	
 					break
@@ -184,13 +143,31 @@ class SearchProblem:
 				return False
 		return True
 
-	def heuristic3(self, goal, node):
-		return self.matrix[node.transport_index[1]][goal]
+	def createAnswer(self, list_nodes):
+		final_list = []
+		for i in range(len(list_nodes[0])):
+			list_for_onepath = []
+			list_indexes = []
+			list_transports = []
+			for j in range(len(list_nodes)):
+				if(i != 0):
+					list_transports.append(list_nodes[j][i].getTransport())
+				list_indexes.append(list_nodes[j][i].getIndex())
+			list_for_onepath.append(list_transports)
+			list_for_onepath.append(list_indexes)
+			final_list.append(list_for_onepath)
 
+		return final_list
+	
+	def reduceTickets(self, transport, tickets):
+		tickets[transport] -= 1
+		return tickets
+
+	#will be able to remove when 3_4 code working for 1_2
 	def heuristic(self, goal, node):
 
-		h2 = self.calculateH2(node.transport_index[0])
-		h3 = self.calculateH3(goal, node.transport_index[1])
+		h2 = self.calculateH2(node.getTransport())
+		h3 = self.calculateH3(goal, node.getIndex())
 		
 		return h2 + h3
 
@@ -208,22 +185,17 @@ class SearchProblem:
 		y1 = self.auxheur[node - 1][1]
 		y2 = self.auxheur[goal - 1][1]
 		return math.sqrt((x2 - x1)**2 + (y2-y1)**2) / 120
-	
-	def neighbors(self, index):
-		return self.model[index] 
 
-	def reduceTickets(self, transport, tickets):
-		tickets[transport] -= 1
-		return tickets
+
 class Node:
+
 	def __init__(self, transport_index, father, tickets):
 		self.transport_index = transport_index
 		self.father = father
-		self.tickets = tickets.copy()
+		self.tickets = tickets.copy() #after 1 and 2 are in 3_4 it can be gone
 		
 		self.g = 0
 		self.h = 0
-		self.f = 0
 	
 	def __repr__(self):
 		return str(self.transport_index[1])
@@ -231,18 +203,15 @@ class Node:
 	def getFather(self):
 		return self.father
 
+	def setFather(self, father):
+		self.father = father
+
 	def getIndex(self):
-		return self.index
-		
-	def getG(self):
-		return self.g
-		
-	def getH(self):
-		return self.h 
+		return self.transport_index[1]
 	
-	def getF(self):
-		return self.f
-	
+	def getTransport(self):
+		return self.transport_index[0]
+
 	def reduceTickets(self, transport):
 		self.tickets[transport] -= 1
 
@@ -250,11 +219,15 @@ class Node:
 
 def floydWarshall(model):
 	path_deep = []
+
+	#initialize matrix
 	for i in range(len(model)):
 		path_deep.append([])
 		for j in range(len(model)):
-			path_deep[i].append(math.inf)
+			path_deep[i].append(math.inf) 
 	
+	
+	#insert depth of all connections in matrix
 	for i in range(len(model)):
 		for j in range(len(model[i])):
 			path_deep[i][model[i][j][1]] = 1
@@ -266,7 +239,3 @@ def floydWarshall(model):
 					path_deep[i][j] = path_deep[i][k] + path_deep[k][j] 
 
 	return path_deep
-
-def printFloyd(lst):
-	for i in range(len(lst)):
-		print(str(i) + " " +str(lst[i]))
